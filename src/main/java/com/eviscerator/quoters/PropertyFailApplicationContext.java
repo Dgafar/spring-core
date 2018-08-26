b1b3ccc3ea8a5636f4903b02c28747e41aff1989
Dgafar <nithogg@rambler.ru>
2018-08-27 02:51:08 +0300

    process new context

--- src/main/java/com/eviscerator/quoters/PropertyFailApplicationContext.java
+++ src/main/java/com/eviscerator/quoters/PropertyFailApplicationContext.java
@@ -1,6 +1,18 @@
 package com.eviscerator.quoters;
 
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
 import org.springframework.context.support.GenericApplicationContext;
 
 public class PropertyFailApplicationContext extends GenericApplicationContext {
+    public PropertyFailApplicationContext(String fileName) {
+        PropertiesBeanDefinitionReader reader = new PropertiesBeanDefinitionReader(this);
+        int i = reader.loadBeanDefinitions(fileName);
+        System.out.println("Found " + i + " beans");
+        refresh();
+    }
+
+    public static void main(String[] args) {
+        PropertyFailApplicationContext context = new PropertyFailApplicationContext("context.properties");
+        context.getBean(Quoter.class).sayQuote();
+    }
 }
