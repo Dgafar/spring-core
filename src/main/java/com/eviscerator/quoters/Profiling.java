398f10ef5ad71289080b0a4218ba0dcc71e2d3fb
Dgafar <nithogg@rambler.ru>
2018-08-27 01:42:08 +0300

    process

---
+++ src/main/java/com/eviscerator/quoters/Profiling.java
@@ -0,0 +1,4 @@
+package com.eviscerator.quoters;
+
+public @interface Profiling {
+}
---
+++ src/main/java/com/eviscerator/quoters/ProfilingControllerMBean.java
@@ -0,0 +1,5 @@
+package com.eviscerator.quoters;
+
+public interface ProfilingControllerMBean {
+    void setEnabled(boolean enabled);
+}
