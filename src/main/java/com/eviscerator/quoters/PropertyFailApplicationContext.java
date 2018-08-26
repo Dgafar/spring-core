a35413b19a0ee6135b22fb119ba4bc2b755d994c
Dgafar <nithogg@rambler.ru>
2018-08-27 02:45:05 +0300

    new Context

---
+++ src/main/java/com/eviscerator/quoters/PropertyFailApplicationContext.java
@@ -0,0 +1,6 @@
+package com.eviscerator.quoters;
+
+import org.springframework.context.support.GenericApplicationContext;
+
+public class PropertyFailApplicationContext extends GenericApplicationContext {
+}
